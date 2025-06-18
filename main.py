04f57ccef2cd4f4205642e14b35245471ca82c3f
Jayvien Mocallay <j.mocallay.141340.tc@umindanao.edu.ph>
2025-06-18 22:19:04 +0800

    initializing main app

---
+++ main.py
@@ -0,0 +1,14 @@
+from parser import parse_logs
+from db import init_db, save_suspicious_logs
+
+LOG_FILE= "logs/sample_auth.log"
+
+def main():
+    init_db()
+    suspicious_entries = parse_logs(LOG_FILE)
+    for entry in suspicious_entries:
+        print("[!] That's SUS: ", entry)
+        save_suspicious_logs(entry)
+
+if __name__ == "__main__":
+    main()
